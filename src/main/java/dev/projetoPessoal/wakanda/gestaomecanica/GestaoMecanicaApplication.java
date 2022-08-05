42285bafbc0a73307d539cb829d424e2ebdcc56d
joan <https://github.com/wakander-joan/Teste.git>
2022-08-04 22:45:13 -0300

    Primeiro commit prejeto nem foi aberto ainda!

---
+++ src/main/java/dev/projetoPessoal/wakanda/gestaomecanica/GestaoMecanicaApplication.java
@@ -0,0 +1,13 @@
+package dev.projetoPessoal.wakanda.gestaomecanica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoMecanicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoMecanicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/projetoPessoal/wakanda/gestaomecanica/GestaoMecanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projetoPessoal.wakanda.gestaomecanica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoMecanicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
