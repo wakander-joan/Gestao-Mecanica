f28219167b047dee26e2b35877ec57fa9caaac51
joan <https://github.com/wakander-joan/Teste.git>
2022-08-07 10:59:22 -0300

    chamada da api no navegador configurado

--- src/main/java/br/com/wakanda/gestaomecanica/GestaoMecanicaApplication.java
+++ src/main/java/br/com/wakanda/gestaomecanica/GestaoMecanicaApplication.java
@@ -2,9 +2,20 @@ package br.com.wakanda.gestaomecanica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class GestaoMecanicaApplication {
+	
+	@GetMapping
+	public String getHomeTeste() {
+		 
+		return "<h1>Primeiro passo bem sucedido Joan!</h1>";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoMecanicaApplication.class, args);
